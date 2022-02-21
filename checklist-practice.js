621d6595824ed9184e3039e6783e343cb1a40abb
khanshorif331 <khanshorif331@gmail.com>
2022-02-21 21:09:49 +0600

    more problem solved

--- checklist-practice.js
+++ checklist-practice.js
@@ -53,13 +53,21 @@ const multiplyThree = (x,y,z) => x * y * z;
 
 // ৩.৪ এইবার দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
 
-
+const addMultiple = (x,y) => {
+    const add = x+2;
+    const add2 = y+2;
+    const result2 = add * add2;
+    return result2;
+}
+// const examine = addMultiple(8,6)
+// console.log(examine);
 
 
 
 
 
 // ৪. [হোম ওয়ার্ক] একটু গুগলে সার্চ দাও: javascript function declaration vs arrow function তারপর কয়েকটা আর্টিকেল পড়ে বুঝার চেষ্টা করো। 
+
 
 
 
@@ -74,42 +82,73 @@ const multiplyThree = (x,y,z) => x * y * z;
 
 // ৫. অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৫ দিয়ে গুন্ করে গুনফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে। 
 
+const arrayNumbers = [21,35,45,67,48,54,68,25,45,6,3,56,16,63]
+
+const result = arrayNumbers.map(x => x*5)
+// console.log(result);
 
 
 
 // ৬. [ চ্যালেঞ্জিং। গুগলে সার্চ দিয়ে বের করো ] অনেকগুলা সংখ্যার একটা array থেকে শুধু বিজোড় সংখ্যা বের করে নিয়ে আসার জন্য filter ইউজ করো 
 
-
+const array = [21,16,54,23,17,64,51,27,49,91];
+const result3 = array.filter(x => x%2 != 0);
+// console.log(result3);
 
 
 
 // ৭. একটা array এর মধ্যে অনেকগুলা অবজেক্ট আছে। সেখানে যেই অবজেক্ট এর price আছে ৫০০০ টেক্কা সেই অবজেক্টকে find দিয়ে বের করো। 
 
+// const items = [
+//     {name:'phone',color:'white' , price:15000},
+//     {name:'tablet',color:'red' ,price:5600},
+//     {name:'cycle',color:'blue' , price:5000}
+// ];
 
+// // const item = (...items)
+
+// // const find = 
+
+// // const newItems = items.map(x=>x);
+// // const item = (...newItems)=>
+
+// console.log(item.name);
 
 
 // ৭.৫ [এক্সট্রা] জাভাস্ক্রিপ্ট এ array এর map, forEach, filter, find কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো। 
 
+// Arrray map = it gives us all the 
 
 
 
 
-// .
 
 // ৮. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো। 
 
+const items =  {
+    name:'phone',color:'white' , price:15000
+}
 
-
+const {color} = items;
+console.log(color);
+       
 
 // ৯. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+
+const arr = [13,26,32,63,14]
+const [one,two,three] = arr
+// console.log(three)
 
 
 
 // ১০. তিনটা প্যারামিটার ওয়ালা একটা ফাংশন লিখবে। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটার এর যোগ করে যোগফল রিটার্ন করবে। আর থার্ড প্যারামিটার এর একটা ডিফল্ট ভ্যালু থাকবে। সেটা হবে ৭। 
 
+const threeNumber = (x,y,z=7) => x+y+z;
+const value = threeNumber(2,4,25)
+// console.log(value);
 
 
-// .
+
 
 // [অপশনাল]
 
